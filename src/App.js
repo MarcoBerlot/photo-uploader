df3a1f4c633cd1ed1480fecf29c22a6fd7f1eb55
Marco Berlot <marco.berlot@gmail.com>
2018-10-12 18:05:10 -0400

    Setting up react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import FileUpload from './FileUpload'
 import './App.css';
 
+
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <h1>上载</h1>
+            <img height="142" src="https://cdn.dribbble.com/users/285803/screenshots/1066705/dribbble.gif"></img>
+            <p>
+            Upload your pictures!
           </p>
+            <FileUpload/>
           <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
           </a>
+            <h5><p>Made with &hearts; in NYC</p></h5>
         </header>
       </div>
     );
